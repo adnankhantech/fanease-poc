319e1ddf9d867f98d5a37bd7d5f63184947359dc
Adnan <adnan.khan@awrostamani.com>
2019-08-27 20:12:16 +0530

    Fixed issue for css

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, AfterViewInit, OnDestroy} from '@angular/core';
+import {Component, OnInit, AfterViewInit, OnDestroy, ElementRef} from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 
 declare let videojs: any;
@@ -12,22 +12,32 @@ declare let videojs: any;
 export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   private videoJSplayer: any;
 
-  constructor() {  }
+  constructor(private elementRef:ElementRef) { 
+   }
 
   ngOnInit() {}   
 
   ngAfterViewInit(): void {
+    this.videoJSplayer = videojs('video_player');
     this.initVideoJs();
+    this.generateClickEvent();
+
   }
 
+  generateClickEvent(){
+    this.videoJSplayer.on('play' , ()=>{
+      let el =  this.elementRef.nativeElement.querySelector('.rectangle');
+      if(el){
+        el.addEventListener('click', this.viewDetail.bind(this));
+      }
+    })
+  }
   initVideoJs() {
-    this.videoJSplayer = videojs('video_player');
-    
     let overlay_content_first = 
     `<div class="rectangle">
     <img  class="close" src="/assets/images/group-37.png">
     <div class="image-block">
-      <img src="/assets/images/i-logo-3x.png">
+      <img src="/assets/images/i-logo-3x.png" style="width:69px;">
     </div>
     <div class="description">
       <img src="/assets/images/shape.png"><br>
@@ -36,22 +46,68 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     </div>
     </div>`;
     
-    let overlay_content_second = '<p style="color:black;font-size:18px;">M7 Driver</p>'+
-    '<a href="https://www.amazon.in/gp/product/B07HGBMJT6/ref=s9_acss_bw_cg_Topbann_2b1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-4&pf_rd_r=560BEDG3SX66V0XFA00Z&pf_rd_t=101&pf_rd_p=6c0e5a1a-a9c2-441c-968c-513e0354b7a3&pf_rd_i=16613114031" target="_blank" style="color:black;font-size:18px;">View Product</a>'+
-    '</div>';
+    let overlay_content_second = 
+    `<div class="rectangle">
+      <img class="close" src="/assets/images/group-37.png">
+      <div class="image-block">
+          <img src="/assets/images/bitmap.png">
+      </div>
+      <div class="description">
+        <h2 class="M6-Driver">
+          M6 Driver
+        </h2>
+        <p>$ 499.99</p>
+        <img src="/assets/images/group-38.png"><br>
+        <a class="view">
+          View Product >
+        </a>
+      </div>
+    </div>`;
+
+    let overlay_content_third = 
+    `<div class="rectangle">
+      <img class="close" src="/assets/images/group-37.png">
+      <div class="image-block">
+          <img src="/assets/images/tshirt.png">
+      </div>
+      <div class="description">
+        <h2 class="title">
+            NIKE GOLF SHIRT
+        </h2>
+        <p>$ 499.99</p>
+        <img src="/assets/images/nike-logo.png"><br>
+        <a class="view">
+          View Product >
+        </a>
+      </div>
+    </div>`;
 
     this.videoJSplayer.overlay({
         debug: true,
         overlays: [{
             start: 1,
             content: overlay_content_first,
-            end: 'playing',
-            align: 'top-right',
-            class: 'overlay-first'
-        }]
+            end: 5,
+            align: 'top-right'
+        },
+        {
+          start: 5,
+          content: overlay_content_second,
+          end: 10,
+          align: 'bottom-right'
+      },
+      {
+        start: 10,
+        content: overlay_content_third,
+        end: 'playing',
+        align: 'bottom-left'
+    }]
     });
   }
 
+  viewDetail(){
+    alert(1);
+  }
   ngOnDestroy() {
     this.videoJSplayer.dispose();
   }
