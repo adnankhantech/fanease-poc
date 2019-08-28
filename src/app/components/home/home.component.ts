af7fb5dfd3f333516aff27624edea59b76d4ef5e
Adnan <adnan.khan@awrostamani.com>
2019-08-28 14:55:52 +0530

    added detail screen

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,7 +11,7 @@ declare let videojs: any;
 })
 export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   private videoJSplayer: any;
-
+  templateObj:any;
   constructor(private elementRef:ElementRef) { 
    }
 
@@ -19,9 +19,8 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     this.videoJSplayer = videojs('video_player');
-    this.initVideoJs();
+    this.videoJSInit();
     this.generateClickEvent();
-
   }
 
   generateClickEvent(){
@@ -29,80 +28,104 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
       let el =  this.elementRef.nativeElement.querySelector('.rectangle');
       if(el){
         el.addEventListener('click', this.viewDetail.bind(this));
+
       }
     })
   }
-  initVideoJs() {
-    let overlay_content_first = 
-    `<div class="rectangle">
-    <img  class="close" src="/assets/images/group-37.png">
-    <div class="image-block">
-      <img src="/assets/images/i-logo-3x.png" style="width:69px;">
-    </div>
-    <div class="description">
-      <img src="/assets/images/shape.png"><br>
-      <hr>
-      <p>25 &#8451;</p>
-    </div>
-    </div>`;
-    
-    let overlay_content_second = 
-    `<div class="rectangle">
-      <img class="close" src="/assets/images/group-37.png">
-      <div class="image-block">
-          <img src="/assets/images/bitmap.png">
-      </div>
-      <div class="description">
-        <h2 class="M6-Driver">
-          M6 Driver
-        </h2>
-        <p>$ 499.99</p>
-        <img src="/assets/images/group-38.png"><br>
-        <a class="view">
-          View Product >
-        </a>
-      </div>
-    </div>`;
 
-    let overlay_content_third = 
-    `<div class="rectangle">
-      <img class="close" src="/assets/images/group-37.png">
-      <div class="image-block">
-          <img src="/assets/images/tshirt.png">
+  generateTemplateForOverlay() {
+    this.templateObj = {
+      overlay_content_first: `<div class="box1">
+      <div class="rectangle">
+        <img  class="close" src="/assets/images/group-37.png">
+        <div class="image-block">
+          <img src="/assets/images/i-logo-3x.png" style="width:69px;">
+        </div>
+        <div class="description">
+          <img src="/assets/images/shape.png"><br>
+          <hr>
+          <p>25 &#8451;</p>
+        </div>
+        </div>
+        </div>`,
+      overlay_content_second: `<div class="box2">
+        <div class="rectangle ">
+          <img class="close" src="/assets/images/group-37.png">
+          <div class="image-block">
+            <img src="/assets/images/bitmap.png">
+          </div>
+        <div class="description">
+          <h2 class="M6-Driver">
+            M6 Driver
+          </h2>
+          <p>$ 499.99</p>
+          <img src="/assets/images/group-38.png" style="padding-bottom:10px;"><br>
+          <a class="view">
+            View Product >
+          </a>
+        </div>
       </div>
-      <div class="description">
-        <h2 class="title">
-            NIKE GOLF SHIRT
-        </h2>
-        <p>$ 499.99</p>
-        <img src="/assets/images/nike-logo.png"><br>
-        <a class="view">
-          View Product >
-        </a>
+      </div>`,
+      overlay_content_third: `<div class="box3">
+      <div class="rectangle">
+        <img class="close" src="/assets/images/group-37.png">
+        <div class="image-block">
+            <img src="/assets/images/tshirt.png">
+        </div>
+        <div class="description">
+          <h2 class="title">
+              NIKE GOLF SHIRT
+          </h2>
+          <p>$ 499.99</p>
+          <img src="/assets/images/nike-logo.png" style="padding-bottom:10px;"><br>
+          <a class="view">
+            View Product >
+          </a>
+        </div>
       </div>
-    </div>`;
+      </div>`,
+      overlay_content_fourth: `<div class="box4">
+        <div class="rectangle">
+            <img class="close" src="/assets/images/group-37.png">
+            <div class="img-wrap">
+                <img src="/assets/images/group-40@3x.png">
+            </div>
+        </div>
+    </div>`
+    }
+    return this.templateObj;
+  }
 
+  videoJSInit(){
+    let templateObj = this.generateTemplateForOverlay();
     this.videoJSplayer.overlay({
-        debug: true,
-        overlays: [{
-            start: 1,
-            content: overlay_content_first,
-            end: 5,
-            align: 'top-right'
-        },
-        {
-          start: 5,
-          content: overlay_content_second,
-          end: 10,
-          align: 'bottom-right'
+      debug: true,
+      overlays: [{
+          start: 1,
+          content: templateObj.overlay_content_first,
+          end: 5,
+          align: 'top-right'
       },
       {
-        start: 10,
-        content: overlay_content_third,
-        end: 'playing',
-        align: 'bottom-left'
-    }]
-    });
+        start: 5,
+        content: templateObj.overlay_content_second,
+        end: 10,
+        align: 'bottom-right'
+    },
+    {
+      start: 10,
+      content: templateObj.overlay_content_third,
+      end: 15,
+      align: 'bottom-left'
+    },
+    {
+      start: 15,
+      content: templateObj.overlay_content_fourth,
+      end: 'playing',
+      align: 'top-right'
+    },
+    ]
+  });
   }
 
   viewDetail(){
